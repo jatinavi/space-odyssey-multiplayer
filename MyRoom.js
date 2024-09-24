0ee4845f4e4e965ab594045b117ea7c260444174
Jatin kumar Balchandani <89150120+jatinavi@users.noreply.github.com>
2024-09-24 23:47:41 +0530

    Create MyRoom.js

---
+++ MyRoom.js
@@ -0,0 +1,38 @@
+const { Room } = require('colyseus');
+
+class MyRoom extends Room {
+    onCreate(options) {
+        this.setState({
+            players: {}
+        });
+
+        this.onMessage("move", (client, message) => {
+            const player = this.state.players[client.sessionId];
+            if (player) {
+                player.x = message.x;
+                player.y = message.y;
+                player.z = message.z;
+
+                // Broadcast player movement to all other clients
+                this.broadcast("playerMove", {
+                    sessionId: client.sessionId,
+                    name: message.name,  // Avatar_1 or Avatar_2
+                    x: message.x,
+                    y: message.y,
+                    z: message.z
+                });
+            }
+        });
+    }
+
+    onJoin(client) {
+        console.log(client.sessionId, "joined");
+        this.state.players[client.sessionId] = { x: 0, y: 0, z: 0 };  // Initialize player position
+    }
+
+    onLeave(client) {
+        delete this.state.players[client.sessionId];  // Remove player when they leave
+    }
+}
+
+module.exports = MyRoom;
