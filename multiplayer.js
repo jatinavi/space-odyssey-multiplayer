d9850135a41db9a25fc2f49469590376f8c16515
Jatin kumar Balchandani <89150120+jatinavi@users.noreply.github.com>
2024-09-24 23:52:24 +0530

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -11,13 +11,20 @@ Multiplayer.prototype.initialize = function() {
         this.room = room;
         this.sessionId = room.sessionId;
 
+        // Track player position data
+        this.otherPlayers = {};  // Store other players' positions
+        this.interpolationSpeed = 0.1;  // Smoothing for movement
+
         // Determine if this is Avatar_1 or Avatar_2
         this.playerName = this.entity.name;  // Avatar_1 or Avatar_2
 
         // Listen for updates on other players' positions
         room.onMessage("playerMove", (data) => {
             if (data.sessionId !== room.sessionId) {  // If it's not our own sessionId, move the other player
-                this.updatePlayerPosition(data);
+                if (!this.otherPlayers[data.sessionId]) {
+                    this.otherPlayers[data.sessionId] = { entity: this.app.root.findByName(data.name) };
+                }
+                this.otherPlayers[data.sessionId].targetPos = new pc.Vec3(data.x, data.y, data.z);
             }
         });
     }).catch(e => {
@@ -25,18 +32,12 @@ Multiplayer.prototype.initialize = function() {
     });
 };
 
-Multiplayer.prototype.updatePlayerPosition = function(data) {
-    // Update the other player's position in the game
-    var otherPlayer = this.app.root.findByName(data.name);  // Find the other player's avatar by name
-    if (otherPlayer) {
-        otherPlayer.setPosition(data.x, data.y, data.z);  // Update the other player's position
-    }
-};
-
+// Interpolate other players' positions for smoother transitions
 Multiplayer.prototype.update = function(dt) {
-    // Only control our own player
+    var pos = this.entity.getPosition();
+    
+    // Send our position to the server
     if (this.room && this.entity.name === this.playerName) {
-        var pos = this.entity.getPosition();  // Get the current player's position
         this.room.send("move", {
             sessionId: this.sessionId,
             name: this.entity.name,  // Send the avatar name (Avatar_1 or Avatar_2)
@@ -44,5 +45,15 @@ Multiplayer.prototype.update = function(dt) {
             y: pos.y,
             z: pos.z
         });
+    }
+
+    // Interpolate the position of other players
+    for (var sessionId in this.otherPlayers) {
+        var otherPlayer = this.otherPlayers[sessionId];
+        if (otherPlayer.entity && otherPlayer.targetPos) {
+            var currentPos = otherPlayer.entity.getPosition();
+            var newPos = currentPos.lerp(currentPos, otherPlayer.targetPos, this.interpolationSpeed);
+            otherPlayer.entity.setPosition(newPos);
+        }
     }
 };
